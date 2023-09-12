272aa0c9fd24764bd708c49ac2ce69db03ff3477
Nightcycle <77173389+nightcycle@users.noreply.github.com>
2023-09-11 20:01:22 -0400

    Added basic functionality

--- scripts/update-packages.sh
+++ scripts/update-packages.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 wally-update minor
 wally install
+if [ ! -d "Packages" ]; then
+  mkdir "Packages"
+fi
 rojo sourcemap dev.project.json --output sourcemap.json
 wally-package-types --sourcemap sourcemap.json Packages
